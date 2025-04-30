8b9070af3b7df7244007aec15959cf7127396942
Ben <bmg320@icloud.com>
2025-04-30 01:55:40 -0500

    delete

--- QRCodeProj/main.py
+++
@@ -1,7 +0,0 @@
-import qrcode
-url = input("Enter the URL: ")
-img = qrcode.make(url)
-
-filename = input("Enter filename to save QR code (include extension): ")
-img.save(filename)
-print(f"QR code saved as {filename}")
